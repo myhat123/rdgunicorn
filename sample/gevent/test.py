2e0281fe07f26647644f47890f8f91f66cfe6fea
hzg <myhat123@189.cn>
2012-02-19 13:00:41 +0800

    删除docs build目录;调整一下gevent的示例代码

--- sample/gevent/test.py
+++ sample/gevent/test.py
@@ -1,67 +1,31 @@
-# -*- coding: utf8 -*-
-
+from gevent import monkey; monkey.patch_all()
 import gevent
+import urllib2 
 
-#from gevent import joinall,Timeout
-#from gevent import monkey;monkey.patch_all()
-#from gevent.pool import Pool
+def read_url(url):
+    print url
+    response = urllib2.urlopen(url)
+    print response.getcode(), response.msg
 
-import urllib2
-import socket
+def gethostlist():
+    fp = open('iplist.txt', 'r')
 
-CONNECT_TIMEOUT,DATA_TIMEOUT = 10, 10
-IP_COUNT,POOL_SIZE = 1000, 100
+    hostlist = []
 
-def curl(ip):
-    '''
-使用urllib2探测IP是否可以访问，并抽取应答码
-和错误原因
-'''
-    url = 'http://' + ip
-    request = urllib2.Request(url=url)
-    reason, other = None, 0
+    for line in fp.readlines():
+        hostlist.append(r'http://' + line[:-1])
 
-    try:
-        rsp = urllib2.urlopen(request, timeout=10)
-        reason, other = rsp.getcode(), rsp.msg
-    except urllib2.HTTPError, ex:
-        reason, other = ex.code, ex.msg
-    except urllib2.URLError, ex:
-        reason = ex.reason
-        if isinstance(reason, socket.timeout):
-            reason = reason.message
-        elif isinstance(reason, socket.error):
-            reason = reason.strerror
-    finally:
-        print reason, ip, other
-        return reason, ip, other
+    return hostlist
 
-def process_results(results):
-    '''
-处理扫描结果，对结果进行排序并打印，
-及统计各种结果的数量
-'''
-    results = sorted(results)
-    stats = {}
-    for result in results:
-        error = result[0]
-        stats.setdefault(error, 0)
-        stats[error] = stats[error] + 1
-        print result
-    
-    keys = sorted(stats.keys())
-    for key in keys:
-        print key, stats[key]
+def main():
+    hosts = gethostlist()
+    threads = []
+    for x in hosts:
+        a = gevent.spawn(read_url, x)
+        threads.append(a)
+
+    gevent.joinall(threads)
 
 if __name__ == '__main__':
-    iplist = (ip.strip()
-        for i, ip
-        in enumerate(open('iplist.txt', 'r'))
-        if i < IP_COUNT)
+    main()
 
-    #pool = Pool(POOL_SIZE)
-    #jobs = [pool.spawn(curl, ip) for ip in iplist]
-    jobs = [gevent.spawn(curl, ip) for ip in iplist]
-    gevent.joinall(jobs)
-    results = [job.value for job in jobs]
-    process_results(results)
